e455b46aced0ed32c5b2317b73f1ed3465987b9e
buhinalba <buhin.alba@gmail.com>
2020-12-08 14:38:32 +0100

    rafactor: Class names

--- src/main/java/com/codecool/shop/dao/PokemonFilterDao.java
+++ src/main/java/com/codecool/shop/dao/PokemonFilterDao.java
@@ -4,6 +4,6 @@ import com.codecool.shop.model.Pokemon;
 
 import java.util.List;
 
-public interface PokemonFilter extends UtilDao {
+public interface PokemonFilterDao extends UtilDao {
     List<Pokemon> getPokemons();
 }
--- src/main/java/com/codecool/shop/dao/implementation/PokemonFilterDaoByTypeDao.java
+++ src/main/java/com/codecool/shop/dao/implementation/PokemonFilterDaoByTypeDao.java
@@ -1,11 +1,11 @@
 package com.codecool.shop.dao.implementation;
 
-import com.codecool.shop.dao.PokemonFilter;
+import com.codecool.shop.dao.PokemonFilterDao;
 import com.codecool.shop.model.Pokemon;
 
 import java.util.List;
 
-public class PokemonFilterByType implements PokemonFilter {
+public class PokemonFilterDaoByTypeDao implements PokemonFilterDao {
     @Override
     public List<Pokemon> getPokemons() {
         return null;

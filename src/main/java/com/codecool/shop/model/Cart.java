5bd414e7fcd2ea676e76649d7ce908357e305dbf
TheddyDee <nemethmate500@gmail.com>
2020-12-08 22:38:21 +0100

    add, modify, remove cart, cart html started

--- src/main/java/com/codecool/shop/model/Cart.java
+++ src/main/java/com/codecool/shop/model/Cart.java
@@ -1,41 +1,40 @@
 package com.codecool.shop.model;
 
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
 
 public class Cart {
-    private List<Pokemon> pokemons;
-    private int id;
     private String userName;
+    private int orderId;
+    private ArrayList<Pokemon> pokemons;
 
     public Cart() {
+       pokemons = new ArrayList<>();
+       orderId = new Random().nextInt(10000 - 1000) + 1000;
     }
 
-    public Cart(String name) {
-        setName(name);
+    private String getName() {
+        return userName;
     }
 
     private void setName(String name) {
         this.userName = name;
     }
 
-    public Cart(List<Pokemon> pokemons) {
-        setPokemons(pokemons);
-    }
-
     public int getId() {
-        return id;
+        return orderId;
     }
 
     public void setId(int id) {
-        this.id = id;
+        this.orderId = orderId;
     }
 
-    public List<Pokemon> getPokemons() {
+    public ArrayList<Pokemon> getPokemons() {
         return pokemons;
     }
 
-    public void setPokemons(List<Pokemon> pokemons) {
+    public void setPokemons(ArrayList<Pokemon> pokemons) {
         this.pokemons = pokemons;
     }
 
@@ -43,25 +42,35 @@ public class Cart {
         this.pokemons.add(pokemon);
     }
 
-    public void removePokemon(int id) {
-        List<Pokemon> filtered = pokemons.stream()
-                .filter(pokemon -> pokemon.getId() == id)
-                .collect(Collectors.toList());
-        setPokemons(filtered);
+    public void removePokemonFromCart(Pokemon pokemon) {
+        pokemons.remove(pokemon);
     }
 
-    public void removePokemon(String name) {
-        List<Pokemon> filtered = pokemons.stream()
-                .filter(pokemon -> pokemon.getName().equals(name))
-                .collect(Collectors.toList());
-        setPokemons(filtered);
+    public void removeAllCurrentPokemonFromCart(Pokemon pokemon) {
+        int numOfPokemonInCart = howManyOfOne(pokemon);
+        for (int i = 0; i < numOfPokemonInCart; i++) {
+            removePokemonFromCart(pokemon);
+        }
+    }
+
+    public void removeAllPokemonFromCart() {
+        pokemons = new ArrayList<>();
+    }
+
+    public int howManyOfAll() {
+        return this.pokemons.size();
+    }
+
+    public int howManyOfOne(Pokemon pokemon) {
+        if ( pokemons.size() > 0 ) {
+            int count = Collections.frequency(pokemons, pokemon);
+            return count;
+        }
+        return 0;
     }
 
     @Override
     public String toString() {
-        return String.format("id: %1$d, " +
-                        "userName: %2$s, ",
-                this.id,
-                this.userName);
+        return null;
     }
 }

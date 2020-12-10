8b5ebda0d67300922b15c87e333aef4a78ca2cc6
buhinalba <buhin.alba@gmail.com>
2020-12-10 19:50:16 +0100

    change: logging in json

--- src/main/java/com/codecool/shop/controller/OrderLog.java
+++ src/main/java/com/codecool/shop/controller/OrderLog.java
@@ -2,6 +2,7 @@ package com.codecool.shop.controller;
 
 import com.codecool.shop.model.Cart;
 import com.codecool.shop.model.Pokemon;
+import com.google.gson.Gson;
 import org.json.simple.*;
 import java.io.*;
 import java.time.LocalDateTime;
@@ -42,12 +43,16 @@ public class OrderLog {
     public void writeLog(String message, Pokemon pokemon) {
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         LocalDateTime now = LocalDateTime.now();
-        String logMessage = "User " + message + " " + pokemon.getName() + " in cart " + orderId;
+
+        JSONObject log = new JSONObject();
+        log.put("message", message);
+        log.put("date", dtf.format(now));
+        log.put("cartId", orderId);
+        log.put("pokemonName", pokemon.getName());
 
         String fileName = "log_" + orderId + "_" + (dtf.format(now)) + ".json";
-
         try (FileWriter file = new FileWriter(fileName, true)) {
-            file.write(logMessage);
+            file.write(log.toJSONString());
             file.flush();
         } catch (IOException e) {
             e.printStackTrace();

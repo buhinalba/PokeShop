49f98bb0ae585b839df19def18a918ec757a1a46
András Kelle <kelleandras@gmail.com>
2020-12-10 14:05:32 +0100

    outsource EmailHandler to business logic

--- src/main/java/com/codecool/shop/controller/ValidCheckOutController.java
+++ src/main/java/com/codecool/shop/controller/ValidCheckOutController.java
@@ -1,5 +1,6 @@
 package com.codecool.shop.controller;
 
+import com.codecool.shop.business.logic.EmailHandler;
 import com.codecool.shop.config.TemplateEngineUtil;
 import com.codecool.shop.dao.UtilDao;
 import com.codecool.shop.dao.implementation.CartDaoMem;
@@ -8,17 +9,13 @@ import org.thymeleaf.context.WebContext;
 
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
-import javax.mail.internet.MimeMultipart;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
-import java.net.Authenticator;
-import java.net.PasswordAuthentication;
 import java.util.Date;
 import java.util.Properties;
 
@@ -28,31 +25,10 @@ public class ValidCheckOutController extends HttpServlet implements UtilDao {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        EmailHandler emailHandler = new EmailHandler();
         CartDaoMem cartDaoMem = CartDaoMem.getInstance();
 
-        Properties props = new Properties();
-        props.put("mail.smtp.host", "127.0.0.1");
-        props.put("mail.smtp.port", "25");
-        props.put("mail.debug", "true");
 
-        Session session = Session.getDefaultInstance(props);
-        MimeMessage message = new MimeMessage(session);
-
-        try {
-            message.setFrom(new InternetAddress("from@gmail.com"));
-            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse("to@gmail.com"));
-            message.setSubject("Order Confirmation");
-
-            String msg = "<h4>Here comes the content</h4><br>" +
-                    "<p>here comes more, but in next line</p>";
-
-            message.setContent(msg, "text/html");
-            message.setSentDate(new Date());
-
-            Transport.send(message);
-        } catch (MessagingException e) {
-            e.printStackTrace();
-        }
 
     }
 

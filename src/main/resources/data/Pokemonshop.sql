0cbc48945684b28f186bc737b3b7b129ba4f6ef6
buhinalba <buhin.alba@gmail.com>
2021-01-04 16:02:39 +0100

    create: database schema file

---
+++ src/main/resources/data/Pokemonshop.sql
@@ -0,0 +1,74 @@
+alter table if exists POKEMON_CATEGORY drop constraint IF EXISTS fk_pokemon_id CASCADE ;
+alter table if exists POKEMON_CATEGORY drop constraint IF EXISTS fk_category_id CASCADE ;
+drop table if exists POKEMON cascade;
+drop table if exists POKEMON_CATEGORY cascade;
+drop table if exists CATEGORY cascade;
+drop table if exists "USER" cascade;
+drop table if exists CART cascade;
+drop table if exists SHIPPING_INFO cascade;
+drop table if exists PAYMENT_INFO cascade;
+drop table if exists ORDER_HISTORY cascade;
+drop table if exists ORDER_CONTENTS cascade;
+
+
+create table POKEMON (
+  id         int PRIMARY KEY,
+  name       varchar(255), 
+  price      int,
+  sprite_url varchar(255));
+
+
+create table POKEMON_CATEGORY (
+  pokemon_id  int4 not null,
+  category_id int4 not null);
+
+
+create table CATEGORY (
+  id   int PRIMARY KEY,
+  name varchar(255)
+  );
+
+
+create table "USER" (
+  id       int4 not null, 
+  name     varchar(255), 
+  email    varchar(255), 
+  password varchar(255)
+  );
+
+
+create table CART (
+  user_id    int4, 
+  pokemon_id int4, 
+  count      int4);
+
+
+create table SHIPPING_INFO (
+  city    varchar(255), 
+  state   varchar(255), 
+  address varchar(255), 
+  zip     varchar(255));
+
+
+create table PAYMENT_INFO (
+  card_number     int4, 
+  card_name       varchar(255), 
+  expiration_date varchar(255), 
+  cvv             int4);
+
+
+create table ORDER_HISTORY (
+  id       int4 not null, 
+  "date"   timestamp, 
+  user_id  int4, 
+  "Column" int4);
+
+
+create table ORDER_CONTENTS (
+  order_id   int4, 
+  pokemon_id int4, 
+  count      int4);
+
+
+alter table POKEMON_CATEGORY add constraint fk_pokemon_id foreign key (pokemon_id) references POKEMON (id);
+alter table POKEMON_CATEGORY add constraint fk_category_id foreign key (category_id) references CATEGORY (id);

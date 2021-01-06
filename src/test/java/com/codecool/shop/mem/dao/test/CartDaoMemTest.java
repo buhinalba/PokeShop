a9dbbf3138c9abc375aa68d701ed7b3671d89944
Zsolt <mzdzsodzsi@gmail.com>
2021-01-06 09:47:42 +0100

    Test: tests for CarTDaoMem.java

--- src/test/java/com/codecool/shop/mem/dao/test/CartDaoMemTest.java
+++ src/test/java/com/codecool/shop/mem/dao/test/CartDaoMemTest.java
@@ -1,5 +1,74 @@
 package com.codecool.shop.mem.dao.test;
 
+import com.codecool.shop.dao.implementation.CartDaoMem;
+import com.codecool.shop.model.Cart;
+import com.codecool.shop.model.Pokemon;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 class CartDaoMemTest {
 
+    @Test
+    void findPokemon_validPokemonId_returnsEqualsWithPokemon() {
+        Pokemon pokemon = new Pokemon(123, "Charmander", 20, Arrays.asList("normal", "fire!"), "sprite");
+        CartDaoMem cartDaoMem = CartDaoMem.getInstance();
+        cartDaoMem.add(pokemon); // add random pokemon to cart
+        assertEquals(pokemon, cartDaoMem.findPokemon(pokemon.getId()));
+    }
+
+    @Test
+    void findPokemon_invalidPokemonId_throwsNoSuchElementException() {
+        int fakePokemonId = 612;
+        Cart cart = mock(Cart.class);
+        CartDaoMem cartDaoMem = CartDaoMem.getInstance();
+        when(cart.findPokemon(fakePokemonId)).thenThrow(new NoSuchElementException("Pokemon with id " + fakePokemonId + "  not found in cart!"));
+        assertThrows(NoSuchElementException.class, () -> cartDaoMem.findPokemon(fakePokemonId));
+    }
+
+    @Test
+    void getTotalPrice_existingPokemonInCart_returnsTotalPrice() {
+        Pokemon pokemon = new Pokemon(123, "Charmander", 666, Arrays.asList("normal", "fire!"), "sprite");
+        CartDaoMem cartDaoMem = CartDaoMem.getInstance();
+        cartDaoMem.add(pokemon);
+        assertEquals(pokemon.getDefaultPrice(), cartDaoMem.getTotalPrice());
+    }
+
+    @Test
+    void getTotalPrice_existingMultiplePokemonInCart_returnsTotalPrice() {
+        Pokemon pokemon = new Pokemon(123, "Charmander", 666, Arrays.asList("normal", "fire!"), "sprite");
+        CartDaoMem cartDaoMem = CartDaoMem.getInstance();
+        cartDaoMem.add(pokemon);
+        cartDaoMem.add(pokemon);
+        assertEquals(pokemon.getDefaultPrice() * 2, cartDaoMem.getTotalPrice());
+    }
+
+    @Test
+    void getTotalAmount_existingMultiplePokemonInCart_returnsTotalAmount() {
+        Pokemon pokemon = new Pokemon(123, "Charmander", 666, Arrays.asList("normal", "fire!"), "sprite");
+        CartDaoMem cartDaoMem = CartDaoMem.getInstance();
+        cartDaoMem.add(pokemon);
+        cartDaoMem.add(pokemon);
+        cartDaoMem.add(pokemon);
+        assertEquals(3, cartDaoMem.getTotalAmount());
+    }
+
+    @Test
+    void getTotalAmount_nonExistingPokemonInCart_returnsZero() {
+        CartDaoMem cartDaoMem = CartDaoMem.getInstance();
+        assertEquals(cartDaoMem.getTotalAmount(), 0);
+    }
+
+    @Test
+    void getAll_existingPokemonInCart_returnPokemonIntegerHashMap() {
+        Pokemon pokemon = new Pokemon(123, "Charmander", 666, Arrays.asList("normal", "fire!"), "sprite");
+        HashMap<Pokemon, Integer> pokemonIntegerHashMap = new HashMap<>(Map.ofEntries(Map.entry(pokemon, 1))) ;
+        CartDaoMem cartDaoMem = CartDaoMem.getInstance();
+        cartDaoMem.add(pokemon);
+        assertEquals(cartDaoMem.getAll(), pokemonIntegerHashMap);
+    }
 }

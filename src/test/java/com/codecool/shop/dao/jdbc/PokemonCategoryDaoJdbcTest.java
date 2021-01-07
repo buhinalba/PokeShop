badb0a76c3e73c7ee572734cb57e040c7a7253ef
buhinalba <buhin.alba@gmail.com>
2021-01-07 13:56:23 +0100

    add: test classes

---
+++ src/test/java/com/codecool/shop/dao/jdbc/PokemonCategoryDaoJdbcTest.java
@@ -0,0 +1,28 @@
+package com.codecool.shop.dao.jdbc;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PokemonCategoryDaoJdbcTest {
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void find() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void getFullCategoryNameList() {
+    }
+}
---
+++ src/test/java/com/codecool/shop/dao/jdbc/PokemonDaoJdbcTest.java
@@ -0,0 +1,28 @@
+package com.codecool.shop.dao.jdbc;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PokemonDaoJdbcTest {
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void find() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void testGetAll() {
+    }
+}
